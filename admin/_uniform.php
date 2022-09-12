5d32dd860a096df7bc5788cb1714e748734830cb
Aj Narag <ajnarag25@gmail.com>
2022-09-12 23:23:10 +0800

    fix the loader

--- admin/_uniform.php
+++ admin/_uniform.php
@@ -37,12 +37,12 @@
 <body class="theme-blush">
 
 <!-- Page Loader -->
-<!-- <div class="page-loader-wrapper">
+<div class="page-loader-wrapper">
     <div class="loader">
         <div class="m-t-30"><img class="zmdi-hc-spin" src="assets/images/tuplogo.png" width="48" height="48" alt="Aero"></div>
         <p>Please wait...</p>
     </div>
-</div> -->
+</div>
 
 <!-- Overlay For Sidebars -->
 <div class="overlay"></div>
